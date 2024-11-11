67a1fd0adce41bac0d7bce704a37f862c40ae9da
222010301002 <222010301002thanmai@gmail.com>
2024-11-11 23:29:23 +0530

    added the person.js

---
+++ Person.js
@@ -0,0 +1,51 @@
+//Write a “person” class to hold all the details.
+
+// class Person{
+
+//     constructor(name,age,gender,email){
+//         this.name=name
+//         this.age=age
+//         this.gender = gender
+//         this.email = email
+
+//     }
+// }
+
+// let Organs= new Person("Thanmai",22, "MALE", "thanmai@gmail.com ")
+
+// console.log("name:" +Organs.name)
+// console.log("age:" +Organs.age)
+// console.log("gender :" +Organs.gender)
+// console.log("email:" +Organs.email)
+
+
+//write a class to calculate the Uber price.
+// class UberPrice{
+
+//     constructor(baseprice, distance, time){
+
+//         this.baseprice = baseprice;
+//         this.distance = distance;
+//         this.time = time;
+//     }
+
+//         totalamount(){
+//             return this.baseprice + this.distance + this.time
+//         }
+
+    
+
+// }
+
+// let price = new UberPrice(10,20,30);
+
+// console.log("baseprice:"+price.baseprice);
+// console.log("distance:" +price.distance);
+// console.log("time:"+price.time);
+
+
+// let Total = price.totalamount();
+
+// console.log("totalamount:" +Total);
+
+
